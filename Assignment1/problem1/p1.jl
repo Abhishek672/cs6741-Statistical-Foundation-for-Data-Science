ecac95719833c64806e7412cd18e32e8ca9b8616
abhishek negi <abhisheknegi@abhisheks-MacBook-Air.local>
2021-02-15 18:23:38 +0530

    com1

--- Assignment1/problem1/p1.jl
+++ Assignment1/problem1/p1.jl
@@ -3,6 +3,7 @@ using Plots
 n_samples=[5,50,100,200,500,1000,2000,5000,10000,50000,10^5,500000,10^6]
 averages=[]
 using Random
+Random.seed!(4)
 for sample in n_samples
     sum=0
     for _ in 1:sample
@@ -10,7 +11,8 @@ for sample in n_samples
     end
     push!(averages,sum/sample)
 end
-plot(1:length(averages),averages,xlabel="samples taken",
+plot_p1=plot(1:length(averages),averages,xlabel="samples taken",
 ylabel="average",size=(700,600),xticks=(1:length(n_samples),n_samples),
-label="averages",color=:red)
+label="averages",color=:red,grid=false)
+savefig(plot_p1,"/Users/abhisheknegi/Desktop/Stats_4_ds/Assignment1/problem1/a1p1.png")
 ##
