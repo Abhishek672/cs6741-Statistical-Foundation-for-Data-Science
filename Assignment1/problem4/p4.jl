ecccffadff7d3b7056a89c8567f3cc266212bd88
abhishek negi <abhisheknegi@abhisheks-MacBook-Air.local>
2021-02-16 15:29:27 +0530

    changes done

--- Assignment1/problem4/p4.jl
+++ Assignment1/problem4/p4.jl
@@ -1,14 +1,13 @@
 ##
 using Random
-Random.seed!(50)
+Random.seed!(20)
 Dictionary=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T',
 'U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
 'r','s','t','u','v','w','x','y','z','~','!','@', '#', '$', '%', '^', '&', '*', '(', ')',
  '_', '+', '=', '-', '`']
-
 actual_pass=rand(Dictionary,8);
 println(actual_pass)
-n_trails=10^6;
+n_trails=10;
 count=0;
 for _ in 1:n_trails
     guessed_pass=rand(Dictionary,8)
