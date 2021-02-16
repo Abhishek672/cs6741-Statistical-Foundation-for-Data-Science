80b342e50ad5efbe65cfdc99aeb2a51971765454
abhishek negi <abhisheknegi@abhisheks-MacBook-Air.local>
2021-02-16 22:56:01 +0530

    problem6

--- Assignment1/problem6/p6.jl
+++ Assignment1/problem6/p6.jl
@@ -1,7 +1,8 @@
 ##
 using Random
 using Plots
-Random.seed!(0)
+Random.seed!(50)
+
 function Simulate(p,n_trials)
     count=0
     for _ in 1:n_trials
@@ -14,6 +15,8 @@ function Simulate(p,n_trials)
                 net_profit-=1
             end
         end
+
+        #net_profit should be >=0 to ensure the constraint
         if (net_profit>=0)
             count+=1
         end
@@ -22,7 +25,8 @@ function Simulate(p,n_trials)
 end
     
 ##
-n_trails=10^2
+n_trails=10^6
 vals=[Simulate(p,n_trails) for p in 0:0.05:1]
-scatter(0:0.05:1,vals,color=:orange,xlabel="p",ylabel="P(at least 10 rupee after 20 days)",label="probability")
+plot_1=scatter(0:0.05:1,vals,color=:orange,xlabel="p",ylabel="P(at least 10 rupee after 20 days)",label="probability",guidefont=(10))
+savefig(plot1,"\Users\abhisheknegi\Desktop/Stats_4_ds/Assignment1/problem6/a1p6.png")
 ##
